ad83c01957b947ce2d3513b9c58d4b0cd77958a2
manikanta <manikantakondapalli944@gmail.com>
2021-11-09 15:02:05 -0800

    all modified nav-bar

--- src/Components/nav-bar/NavBar.js
+++ src/Components/nav-bar/NavBar.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-4">
         {/* <Link to="/" className="navbar-brand ml-5">React Redux contact app
 
         </Link> */}
-        <a href="/" className="navbar-brand mr-5 ">React Redux Contact App
+        <a href="/" className="navbar-brand fs-2 mx-5">React Redux Contact App
 
         </a>
             
