d713f40dad6fde8e12b7806b8215bde6f77358ec
manikanta <manikantakondapalli944@gmail.com>
2021-11-09 15:23:59 -0800

    after nav-bar commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from 'react';
 
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import NavBar from './Components/nav-bar/NavBar'
 function App() {
   return (
     <div className="App">
+    <Routes>
       <NavBar />
+    {/* <Route path='/' element={<Dashboard/>}/> */}
+    </Routes>
     </div>
+
   );
 }
 
