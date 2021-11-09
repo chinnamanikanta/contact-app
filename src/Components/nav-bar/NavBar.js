faba11121c900bfcdadf5c173631c25798a3f212
manikanta <manikantakondapalli944@gmail.com>
2021-11-09 14:42:24 -0800

    initial navbar

---
+++ src/Components/nav-bar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2">
+        {/* <Link to="/" className="navbar-brand ml-5">React Redux contact app
+
+        </Link> */}
+        <a href="/" className="navbar-brand ml-5">React Redux contact app
+
+        </a>
+            
+        </nav>
+    )
+}
+
+export default NavBar
